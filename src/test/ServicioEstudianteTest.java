597b5273b7f19cf476a197371f30999bbed74f31
Juan Jose Nieto Valencia <nieto.juan.8549@eam.edu.co>
2024-09-05 11:45:28 -0500

    Cambios Karol Barrios

--- src/test/ServicioEstudianteTest.java
+++ src/test/ServicioEstudianteTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import Clases.Estudiante;
 import Clases.EstudiantesRegistrados;
+import Clases.Mascota;
 import Clases.ServicioEstudiante;
 
 class ServicioEstudianteTest {
@@ -34,5 +38,28 @@ class ServicioEstudianteTest {
 		IllegalArgumentException excep=assertThrows(IllegalArgumentException.class,()->servi.esEstudianteRegistrado(null));
 		assertEquals(excep.getMessage(),"No se admiten matrículas nulas o vacías");
 	}
+	@Test
+	void actualizarEstudiantetest(){
+		when(esturegistrados.actualizarEstudiante("pre", estu)).thenReturn(true);
+		boolean acepto= servi.actualizarEstudiante("pre", estu);
+		assertTrue(acepto);
+		
+	}
+	@Test
+	void obtenerPromedioEdadEstudiantestest () {
+		Estudiante estu1=mock(Estudiante.class);
+		Estudiante estu2=mock(Estudiante.class);
+		when(estu1.getEdad()).thenReturn(12);
+		when(estu2.getEdad()).thenReturn(12);
+		List<Estudiante> mockarrayestu=new ArrayList<>();
+		mockarrayestu.add(estu1);
+		mockarrayestu.add(estu2);
+		double edadp= servi.obtenerPromedioEdadEstudiantes();
+		
+		assertEquals(12,edadp);
+		
+		
 	
+		
+	}
 }
